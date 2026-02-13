54d199798dc456f8b7c6971be72e101307248b7f
Henry Glenn <hglenn2@icr-clymene.beocat.ksu.edu>
2026-02-12 21:45:16 -0600

    Initial load_process_control_blocks implimentation

--- src/process_scheduling.c
+++ src/process_scheduling.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
@@ -49,8 +50,75 @@ bool round_robin(dyn_array_t *ready_queue, ScheduleResult_t *result, size_t quan
 
 dyn_array_t *load_process_control_blocks(const char *input_file) 
 {
-	UNUSED(input_file);
-	return NULL;
+	// Validate input value
+	if (input_file == NULL) { return NULL; }
+
+	// Acquire input file descriptor
+	int fd = open(input_file, O_RDONLY);
+	if (fd == -1) { return NULL; }
+
+	// Create a variable to store the number of control blocks in the input file
+	uint32_t control_block_count = 0;
+	uint8_t* count_bytes = (uint8_t*)&control_block_count; // Read friendly pointer
+
+	// Determine the number of control blocks in the input file by reading the first 4 bytes
+	size_t bytes_to_read = sizeof(uint32_t);
+	size_t total_bytes_read = 0;
+	while (bytes_to_read > 0)
+	{
+		ssize_t bytes_read = read(fd, count_bytes + total_bytes_read, bytes_to_read);
+		if (bytes_read > 0)
+		{
+			bytes_to_read -= bytes_read;
+			total_bytes_read += bytes_read;
+		}
+		else if (bytes_read == -1 && errno == EINTR) { continue; }
+		else
+		{
+			close(fd);
+			return NULL;
+		}
+	}
+
+	// Allocate a storage array for the control blocks and read them from the input file
+	size_t input_size = sizeof(ProcessControlBlock_t) * control_block_count;
+	ProcessControlBlock_t* control_block_buffer = malloc(input_size);
+	if (control_block_buffer == NULL)
+	{
+		close(fd);
+		return NULL;
+	}
+
+	// Read all the control blocks into the storage array from the input file
+	uint8_t* buffer_bytes = (uint8_t*)control_block_buffer;
+	bytes_to_read = input_size;
+	total_bytes_read = 0;
+	while (bytes_to_read > 0)
+	{
+		ssize_t bytes_read = read(fd, buffer_bytes + total_bytes_read, bytes_to_read);
+		if (bytes_read > 0)
+		{
+			bytes_to_read -= bytes_read;
+			total_bytes_read += bytes_read;
+		}
+		else if (bytes_read == -1 && errno == EINTR) { continue; }
+		else
+		{
+			free(control_block_buffer);
+			close(fd);
+			return NULL;
+		}
+	}
+
+	// Transfer the control blocks to a dynamic array and free the temperary buffer
+	dyn_array_t* control_blocks = dyn_array_import(control_block_buffer, control_block_count, sizeof(ProcessControlBlock_t), NULL);
+	free(control_block_buffer);
+
+	// Close the file
+	close(fd);
+
+	// Return control blocks
+	return control_blocks;
 }
 
 bool shortest_remaining_time_first(dyn_array_t *ready_queue, ScheduleResult_t *result) 
