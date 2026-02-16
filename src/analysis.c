62f13055985f9fef6ee5007e97d8747de59c8e6a
Henry Glenn <hglenn2@icr-helios.beocat.ksu.edu>
2026-02-16 15:03:44 -0600

    Added analysis.c implimentation and fixed CMakeLists.txt linking

--- src/analysis.c
+++ src/analysis.c
@@ -1,5 +1,9 @@
+#define _GNU_SOURCE
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "dyn_array.h"
 #include "processing_scheduling.h"
@@ -20,7 +24,64 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	abort();		// REPLACE ME with implementation.
+	// Assign and validate argument values
+	char* filename = argv[0];
+	char* algorithm_name = argv[1];
+	if (filename == NULL || algorithm_name == NULL) { return EXIT_FAILURE; }
 
-	return EXIT_SUCCESS;
+	// Assign optional argument
+	size_t quantum = 0;
+	if (argc > 3 && sscanf(argv[2], "%zu", &quantum) <= 0) { return EXIT_FAILURE; }
+
+	// Extract schedule data from the provided file
+	dyn_array_t* schedule = load_process_control_blocks(filename);
+	if (schedule == NULL) { return EXIT_FAILURE; }
+
+	// Allocate storage for the schedule algorithm result
+	ScheduleResult_t* result = malloc(sizeof(ScheduleResult_t));
+	if (result == NULL) { return EXIT_FAILURE; }
+
+	// Call the correct scheduling algorithm on the provided file schedule data
+	static const int MAX_ALGORITHM_NAME_LENGTH = 5;
+	switch (strnlen(algorithm_name, MAX_ALGORITHM_NAME_LENGTH))
+	{	
+		case 1:
+			if (strncmp(algorithm_name, P, 1) == 0)
+			{
+				priority(schedule, result);
+				return EXIT_SUCCESS;
+			}
+        	break;
+		
+		case 2:
+			if (strncmp(algorithm_name, RR, 2) == 0)
+			{
+				if (quantum == 0) { return EXIT_FAILURE; }
+				round_robin(schedule, result, quantum);
+				return EXIT_SUCCESS;
+			}
+        	break;
+
+		case 3:
+			if (strncmp(algorithm_name, SJF, 3) == 0)
+			{
+				shortest_job_first(schedule, result);
+				return EXIT_SUCCESS;
+			}
+			else if (strncmp(algorithm_name, SRT, 3) == 0)
+			{
+				shortest_remaining_time_first(schedule, result);
+				return EXIT_SUCCESS;
+			}
+			break;
+
+		case 4:
+			if (strncmp(algorithm_name, FCFS, 4) == 0)
+			{
+				first_come_first_serve(schedule, result);
+				return EXIT_SUCCESS;
+			}
+			break;
+	}
+	return EXIT_FAILURE;
 }
